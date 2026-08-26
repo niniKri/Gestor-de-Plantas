8348f085f7767283b6a34d49a9896fc756445394
Anny <annykristel45@gmail.com>
2026-08-26 11:03:17 +0100

    Adicionada funcao guardar dados

--- Gestor_plantas.py
+++ Gestor_plantas.py
@@ -5,14 +5,13 @@ from datetime import datetime, date
 
 FICHEIRO = "plantas.json"
 
-# ============================================================
-#  FUNÇÃO: CARREGAR DADOS -> Guarda plantas e as contas 
-# ============================================================
+# ========================================================================================================================
+# 1 - FUNÇÃO: CARREGAR DADOS -> Guarda plantas e as contas 
+# ========================================================================================================================
 
 #Esta função serve para carregar os dados guardados no ficheiro JSON. 
 #Primeiro, tenta abrir e ler o ficheiro. 
 #Depois, verifica se os dados estão no formato correto e garante que existem as listas de utilizadores e plantas.
-#Caso o ficheiro não exista, esteja inválido ou aconteça algum erro ao abri-lo, a função apresenta uma mensagem e inicia a aplicação com listas vazias.
 
 def carregar_dados():
     try:
@@ -20,7 +19,7 @@ def carregar_dados():
             dados = json.load(ficheiro)
 
             if not isinstance(dados, dict):
-                print("Erro: o ficheiro JSON está inválido.")
+                print("Erro: o ficheiro JSON está inválido.") # Possivel erro : Ficheiro JSON invalido
                 return {"utilizadores": [], "plantas": []}
 
             if "utilizadores" not in dados:
@@ -32,15 +31,35 @@ def carregar_dados():
             return dados
 
     except FileNotFoundError:
-        print("O ficheiro plantas.json ainda não existe.")
+        print("O ficheiro plantas.json ainda não existe.")  # Possivel erro : Ficheiro JSON nao existe
         print("Será criado automaticamente.")
         return {"utilizadores": [], "plantas": []}
 
     except json.JSONDecodeError:
-        print("Erro: o ficheiro plantas.json contém dados inválidos.")
+        print("Erro: o ficheiro plantas.json contém dados inválidos.") # Possivel erro : Ficheiro JSON com dados invalidos
         print("A aplicação vai começar sem dados.")
         return {"utilizadores": [], "plantas": []}
 
     except OSError:
-        print("Erro ao abrir o ficheiro de dados.")
+        print("Erro ao abrir o ficheiro de dados.")  # Possivel erro : Erro ao abrir o Ficheiro JSON 
         return {"utilizadores": [], "plantas": []}
+
+# ========================================================================================================================
+# 2 - FUNÇÃO: GUARDAR DADOS
+# ========================================================================================================================
+
+#Esta função serve para guardar os dados da aplicação no ficheiro JSON. 
+#Recebe os dados através do parâmetro dados, abre o ficheiro em modo de escrita e grava os dados no formato JSON. 
+
+def guardar_dados(dados):
+    try:
+        with open(FICHEIRO, "w", encoding="utf-8") as ficheiro:
+            json.dump(
+                dados,
+                ficheiro,
+                indent=4,
+                ensure_ascii=False
+            )
+
+    except OSError:
+        print("Erro: não foi possível guardar os dados.")  # Possivel erro : Nao foi possivel guardar os dados
